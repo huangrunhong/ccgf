bfb876e4a19c1d53bb54127a70215a22cc1af0d6
Runhong Huang <huang.run.hong.1986@gmail.com>
2025-07-20 17:18:53 +0200

    Add open graph

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,6 +8,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ __($title) }} - {{ __('title') }}</title>
     <link rel="icon" type="image/x-icon" href="/assets/favicon.ico?">
+    <meta property="og:title" content="{{ __($title) }} - {{ __('title') }}" />
+    <meta property="og:description" content="领受福音的好处, 结出福音的果子" />
+    <meta property="og:url" content="https://ccgf.de/{{ app()->getLocale() }}" />
+    <meta property="og:image" content="/assets/images/open-graph.jpeg" />
     <link
         href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet">
